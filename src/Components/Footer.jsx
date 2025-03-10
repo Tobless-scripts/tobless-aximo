69b61e22761db1fb5262c86423b99d71e839ab5e
Tobless-scripts <obayomitaofeek7@gmail.com>
2025-03-10 22:13:43 +0100

    Update code

---
+++ src/Components/Footer.jsx
@@ -0,0 +1,102 @@
+import "../index.css";
+import send from "../assets/send.png";
+import logo from "../../public/logo.svg";
+
+function Footer() {
+    return (
+        <>
+            <div className="max-lg:py-8 max-lg:px-2 max-md:py-16 px-25 pt-25 pb-5 bg-black text-[#fdfde1] flex flex-col gap-8">
+                <div className="grid grid-cols-2 gap-12  max-lg:grid-cols-1 border-b-[0.00000001rem] border-[#fdfde1] pb-28 ">
+                    <div className="p-2 flex flex-col gap-6 max-lg:px-8">
+                        <h1 className="leading-[4.6875rem] text-[4.375rem] font-bold max-md:font-[600] max-md:leading-[2.4375rem] max-md:text-[2.25rem]  ">
+                            Lets start a project together
+                        </h1>
+                        <p className=" leading-[1.875rem] text-[1.3125rem] font-normal">
+                            We work closely with our clients to understand their
+                            objectives, target audience, and unique needs. We
+                            use our creative skills to translate these
+                            requirements and practical design solutions.
+                        </p>
+                        <div className="flex flex-col gap-2">
+                            <div className="flex flex-col gap-1">
+                                <h2 className="text-lime-300 font-normal leading-[1.875rem] text-[1.3125rem] ">
+                                    Give us a call:
+                                </h2>
+                                <p className="font-normal leading-[1.875rem] text-[1.3125rem] ">
+                                    (123) 456-7890
+                                </p>
+                            </div>
+                            <div className="flex flex-col gap-1">
+                                <h2 className="text-lime-300 font-normal leading-[1.875rem] text-[1.3125rem] ">
+                                    Send us an email:
+                                </h2>
+                                <p className="font-normal leading-[1.875rem] text-[1.3125rem] ">
+                                    info@mthemeus.com
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="py-2 pl-8 flex flex-col gap-6 max-lg:pr-8">
+                        <h1 className="font-bold leading-[1.75rem] text-[1.5rem] ">
+                            Send us a message
+                        </h1>
+                        <form
+                            className="flex flex-col gap-6"
+                            action="https://formspree.io/f/xpwwkjea"
+                            method="post"
+                        >
+                            <input
+                                type="text"
+                                name="name"
+                                placeholder="Enter your name"
+                                required
+                                className="w-[90%] rounded-full border-[#fdfde1] border-1 p-4 max-lg:w-[100%]"
+                            />
+                            <input
+                                type="email"
+                                name="email"
+                                placeholder="Your email address"
+                                required
+                                className="w-[90%] rounded-full border-[#fdfde1] border-1 p-4 max-lg:w-[100%]"
+                            />
+                            <input
+                                type="number"
+                                name="number"
+                                placeholder="+088-234-6849"
+                                required
+                                className="w-[90%] rounded-full border-[#fdfde1] border-1 p-4 max-lg:w-[100%]"
+                            />
+                            <textarea
+                                name="message"
+                                id="message"
+                                placeholder="Write your message here"
+                                className="w-[90%] rounded-xl border-[#fdfde1] border-1 px-4 py-8 max-lg:w-[100%]"
+                            ></textarea>
+
+                            <div className="inline-flex w-[90%] items-center text-center justify-between py-2.5 pl-12 pr-4 border-2 font-bold text-base border-black rounded-full font-inter transition duration-500 bg-lime-300 text-black cursor-pointer shadow-white shadow-md hover:shadow-none max-sm:w-full max-sm:pl-6 max-sm:pr-2">
+                                <div>
+                                    <p>Book a free consultation</p>
+                                </div>
+                                <div className="w-12 bg-black p-2 rounded-full h-12 ">
+                                    <img
+                                        src={send}
+                                        alt="phone"
+                                        className="w-full"
+                                    />
+                                </div>
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <div className="flex space-between w-[full]">
+                    <img src={logo} alt="logo" />
+                    <h2>
+                        &copy; Copyright 2024, All Rights Reserved by Mthemeus
+                    </h2>
+                </div>
+            </div>
+        </>
+    );
+}
+
+export default Footer;
--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -13,7 +13,16 @@ import course4 from "../assets/motion.png";
 import next from "../assets/next.svg";
 import thumbnail from "../assets/video-thumbnail.png";
 import play from "../assets/play button.svg";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import open from "../assets/open.png";
+import close from "../assets/close.png";
+import stars from "../assets/star.png";
+import flower from "../assets/flower.png";
+import member1 from "../assets/team1-DmWE10zh.png";
+import member2 from "../assets/member2.png";
+import member3 from "../assets/member3.png";
+import member4 from "../assets/member4.png";
+import Footer from "./Footer";
 
 function Home() {
     return (
@@ -24,6 +33,10 @@ function Home() {
             <Outstanding />
             <Projects />
             <Quality />
+            <Clients />
+            <VerticalScrollText />
+            <Team />
+            <Footer />
         </>
     );
 }
@@ -119,7 +132,7 @@ function HeroSection() {
 function Provide() {
     return (
         <>
-            <div className="p-25 max-md:py-3 max-md:px-2">
+            <div className="p-25 max-lg:py-3 max-lg:px-2">
                 <div className="text-center px-12 pt-12 max-md:px-0">
                     <h1 className="leading-[4.6875rem] text-[4.375rem] font-bold max-md:font-[600] max-md:leading-[2.4375rem] max-md:text-[2.25rem] ">
                         We provide effective design solutions
@@ -203,14 +216,14 @@ function Provide() {
 function Outstanding() {
     return (
         <>
-            <div className="p-25 max-md:py-3 max-md:px-2 mt-25">
+            <div className="p-25 max-lg:py-6 max-lg:px-4 mt-25">
                 <div className="grid grid-cols-2 gap-8 items-center max-md:grid-cols-1 max-md:text-center">
                     <div>
                         <h1 className="font-bold leading-[4.6875rem] text-[4rem] max-md:font-[600] max-md:leading-[2.4375rem] max-md:text-[2.25rem] ">
                             We make your business stand out
                         </h1>
                     </div>
-                    <div className="pl-48 max-md:pl-0">
+                    <div className="pl-48 max-lg:pl-0">
                         <p className="font-[rgb(12, 12, 12)] leading-[1.875rem] text-[1.3125rem] font-[400] ">
                             We work closely with our clients to know their
                             objectives, target audience, unique needs, and
@@ -226,7 +239,7 @@ function Outstanding() {
                             className="w-full h-full"
                         />
 
-                        <div className="absolute top-[50%] left-[40%] cursor-pointer shadow-white shadow-md hover:shadow-none rounded-full duration-300 ease-in-out ">
+                        <div className="absolute top-[50%] left-[40%] max-sm:left-[25%] max-md:left-[35%]  cursor-pointer shadow-white shadow-md hover:shadow-none rounded-full duration-300 ease-in-out ">
                             <img src={play} alt="play" />
                         </div>
                     </div>
@@ -283,9 +296,12 @@ function Quality() {
     const toggleSection = (section) => {
         setOpenSection(openSection === section ? null : section);
     };
+    useEffect(() => {
+        toggleSection("idea");
+    }, []);
     return (
         <>
-            <div className="grid grid-cols-2 p-25 gap-8 max-md:grid-cols-1 max-md:py-3 max-md:px-2 max-md:text-center">
+            <div className="grid grid-cols-2 p-25 gap-8 max-lg:grid-cols-1 max-md:py-3 max-md:px-2 max-md:text-center">
                 <div>
                     <h1 className="leading-[4.5rem] text-[3.9rem] font-bold max-md:font-[600] max-md:leading-[2.4375rem] max-md:text-[2.25rem] text-black py-4">
                         Our high-quality working processes
@@ -305,16 +321,23 @@ function Quality() {
                 <div className="p-5 space-y-3 justify-center flex flex-col items-center max-md:text-left">
                     {/* Project Idea */}
                     <div
-                        className={`px-8 py-6 border-2 rounded-lg cursor-pointer transition-all duration-500 ease-in-out w-[80%] max-md:w-[100%]  ${
+                        className={`px-8 py-6 border-2 rounded-lg cursor-pointer transition-all duration-500 ease-in-out w-[98%] max-md:w-[100%]  ${
                             openSection === "idea"
                                 ? "bg-lime-300 shadow-lg shadow-black"
                                 : "bg-white shadow-none"
                         }`}
                         onClick={() => toggleSection("idea")}
                     >
-                        <h1 className="font-bold leading-[2.625rem] text-[1.875rem] ">
-                            01/ Project Idea
-                        </h1>
+                        <div className="flex justify-between items-center">
+                            <h1 className="font-bold leading-[2.625rem] text-[1.875rem] max-md:text-[1.5rem] max-md:leading-[2.3rem] ">
+                                01/Project Idea
+                            </h1>
+                            <img
+                                src={openSection === "idea" ? close : open}
+                                alt="faq"
+                                className="w-[2rem] "
+                            />
+                        </div>
                         {openSection === "idea" && (
                             <p className="mt-2 font-normal text-[rgb(33, 37, 41)] text-[1.125rem] leading-[1.875rem] ">
                                 The process starts with a detailed discussion
@@ -326,18 +349,29 @@ function Quality() {
 
                     {/* Brainstorming */}
                     <div
-                        className={`p-5 border-2 rounded-lg cursor-pointer transition-all duration-500 ease-in-out w-[80%] max-md:w-[100%] ${
+                        className={`p-5 border-2 rounded-lg cursor-pointer transition-all duration-500 ease-in-out w-[98%] max-md:w-[100%] ${
                             openSection === "brainstorming"
                                 ? "bg-lime-300 shadow-lg shadow-black"
                                 : "bg-white shadow-none"
                         }`}
                         onClick={() => toggleSection("brainstorming")}
                     >
-                        <h1 className="font-bold leading-[2.625rem] text-[1.875rem] ">
-                            02/ Brainstorming
-                        </h1>
+                        <div className="flex justify-between items-center">
+                            <h1 className="font-bold leading-[2.625rem] text-[1.875rem]  max-md:text-[1.5rem] max-md:leading-[2.3rem] ">
+                                02/Brainstorming
+                            </h1>
+                            <img
+                                src={
+                                    openSection === "brainstorming"
+                                        ? close
+                                        : open
+                                }
+                                alt="faq"
+                                className="w-[2rem] "
+                            />
+                        </div>
                         {openSection === "brainstorming" && (
-                            <p className="mt-2 font-normal text-[rgb(33, 37, 41)] text-[1.125rem] leading-[1.875rem] ">
+                            <p className="mt-2 font-normal text-[rgb(33, 37, 41)] text-[1.125rem] leading-[1.875rem]  ">
                                 Brainstorming is a group creativity technique in
                                 which members attempt to find a solution
                             </p>
@@ -346,16 +380,23 @@ function Quality() {
 
                     {/* Launch */}
                     <div
-                        className={`p-5 border-2 rounded-lg cursor-pointer transition-all duration-500 ease-in-out w-[80%] max-md:w-[100%] ${
+                        className={`p-5 border-2 rounded-lg cursor-pointer transition-all duration-500 ease-in-out w-[98%] max-md:w-[100%] ${
                             openSection === "launch"
                                 ? "bg-lime-300 shadow-lg shadow-black"
                                 : "bg-white shadow-none"
                         }`}
                         onClick={() => toggleSection("launch")}
                     >
-                        <h1 className="font-bold leading-[2.625rem] text-[1.875rem] ">
-                            03/ Launch
-                        </h1>
+                        <div className="flex justify-between items-center">
+                            <h1 className="font-bold leading-[2.625rem] text-[1.875rem]  max-md:text-[1.5rem] max-md:leading-[2.3rem] ">
+                                03/ Launch
+                            </h1>
+                            <img
+                                src={openSection === "launch" ? close : open}
+                                alt="faq"
+                                className="w-[2rem] "
+                            />
+                        </div>
                         {openSection === "launch" && (
                             <p className="mt-2 font-normal text-[rgb(33, 37, 41)] text-[1.125rem] leading-[1.875rem] ">
                                 The completed design assets or final product are
@@ -363,6 +404,182 @@ function Quality() {
                             </p>
                         )}
                     </div>
+                </div>
+            </div>
+        </>
+    );
+}
+
+function Clients() {
+    const clients = [
+        {
+            id: 1,
+            ReviewTopic: "Super customer service!",
+            Review: "Excellent customer service and I was really impressed and happy with my purchase especially as it was a last minute order which got to me in time, and when it arrived I was very happy with the design and size and so was the recipient.",
+            src: img1,
+            Name: "William Jack",
+            Profession: "Founder@XYZ",
+        },
+
+        {
+            id: 2,
+            ReviewTopic: "Exceptional creativity and vision",
+            Review: "Working Mthemeus was a game-changer for our brand. Their exceptional creativity & vision breathed new life into our visual. The logo they perfectly captures our essence & has become instantly recognizable. We couldn't be happier the results!",
+            src: img2,
+            Name: "Smith Align",
+            Profession: "Businessman",
+        },
+
+        {
+            id: 3,
+            ReviewTopic: "Innovative and professional",
+            Review: "I can't say enough good things about them. Their team is not only incredibly talented but also highly professional. They listened to our ideas and brought to life in ways we couldn't have imagined. Their innovative approach and dedication to our project.",
+            src: img3,
+            Name: "Smith Align",
+            Profession: "Milano Joe",
+        },
+
+        {
+            id: 4,
+            ReviewTopic: "Transformed our brand",
+            Review: "Our partnership with Mthemeus transformed our brand from ordinary to extraordinary. Their branding expertise and design work elevated our marketing materials to a whole new level. Our customers have taken notice, and boost in brand recognition.",
+            src: img1,
+            Name: "Daniel Mark",
+            Profession: "Marketing Director",
+        },
+    ];
+    return (
+        <>
+            <div className="p-25 max-md:py-3 max-md:px-2 ">
+                <div className="text-center px-25 pt-12 max-md:px-0">
+                    <h1 className="leading-[4.6875rem] text-[4.375rem] font-bold max-md:font-[600] max-md:leading-[2.4375rem] max-md:text-[2.25rem] ">
+                        Clients are always satisfied with us
+                    </h1>
+                </div>
+                <div className="py-8 grid grid-cols-2 gap-8 max-lg:grid-cols-1 ">
+                    {clients.map((client) => (
+                        <div
+                            key={client.id}
+                            className="p-8 border-2 border-black rounded-xl hover:shadow-lg hover:shadow-black transition-all duration-500 ease-in-out cursor-pointer "
+                        >
+                            <img
+                                src={stars}
+                                alt="stars"
+                                className="w-[11rem] pb-2 "
+                            />
+                            <h1 className="font-bold leading-[2.625rem] text-[2rem] pb-2 ">
+                                {client.ReviewTopic}
+                            </h1>
+                            <p className="font-normal text-[1.125rem] leading-[1.875rem] ">
+                                {client.Review}
+                            </p>
+                            <div className="flex justify-start gap-8 mt-8">
+                                <div className="rounded-full border-2 border-black w-[4rem] h-[4rem] overflow-hidden ">
+                                    <img
+                                        src={client.src}
+                                        alt="Client"
+                                        className="w-full"
+                                    />
+                                </div>
+                                <div>
+                                    <h2 className="font-bold leading-[1.875rem] text-[1.3125rem] ">
+                                        {client.Name}
+                                    </h2>
+                                    <p className="font-normal leading-[1.875rem] text-[1.3125rem] ">
+                                        {client.Profession}
+                                    </p>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
+                </div>
+            </div>
+        </>
+    );
+}
+
+function VerticalScrollText() {
+    return (
+        <div className="scroll-container">
+            <div className="scroll-text p-8">
+                <div className="flex gap-2 mx-14">
+                    <img src={flower} alt="flower" />
+                    <h1 className="text-center text-[2.1875rem] leading-[3.0625rem] font-bold px-4 max-md:font-[600] max-md:leading-[2.5rem] max-md:text-[1.9rem]">
+                        LET'S CREATE NEW EXPERIENCES
+                    </h1>
+                    <img src={flower} alt="flower" />
+                </div>
+
+                <div className="flex gap-2 mx-14">
+                    <h1 className="text-center text-[2.1875rem] leading-[3.0625rem] font-bold px-4">
+                        LET'S CREATE NEW EXPERIENCES
+                    </h1>
+                    <img src={flower} alt="flower" />
+                </div>
+
+                <div className="flex gap-2 mx-14">
+                    <h1 className="text-center text-[2.1875rem] leading-[3.0625rem] font-bold px-4">
+                        LET'S CREATE NEW EXPERIENCES
+                    </h1>
+                    <img src={flower} alt="flower" />
+                </div>
+
+                <div className="flex gap-2 mx-14">
+                    <h1 className="text-center text-[2.1875rem] leading-[3.0625rem] font-bold px-4">
+                        LET'S CREATE NEW EXPERIENCES
+                    </h1>
+                    <img src={flower} alt="flower" />
+                </div>
+
+                <div className="flex gap-2 mx-14">
+                    <h1 className="text-center text-[2.1875rem] leading-[3.0625rem] font-bold px-4">
+                        LET'S CREATE NEW EXPERIENCES
+                    </h1>
+                    <img src={flower} alt="flower" />
+                </div>
+
+                <div className="flex gap-2 mx-14">
+                    <h1 className="text-center text-[2.1875rem] leading-[3.0625rem] font-bold px-4">
+                        LET'S CREATE NEW EXPERIENCES
+                    </h1>
+                    <img src={flower} alt="flower" />
+                </div>
+            </div>
+        </div>
+    );
+}
+
+function Team() {
+    const members = [
+        { src: member1, Name: "Andrew Mark", Role: "Creative Director", id: 1 },
+        { src: member2, Name: "Jack Taylor", Role: "Senior Designer", id: 2 },
+        { src: member3, Name: "Martine Joy", Role: "Project Manager", id: 3 },
+        { src: member4, Name: "Adam Straw", Role: "Web Developer", id: 4 },
+    ];
+    return (
+        <>
+            <div className="p-25 max-md:py-3 max-md:px-2 ">
+                <div className="text-center px-25 pt-12 max-lg:px-0">
+                    <h1 className="leading-[4.6875rem] text-[4.375rem] font-bold max-md:font-[600] max-md:leading-[2.4375rem] max-md:text-[2.25rem] ">
+                        We have a team of creative people
+                    </h1>
+                </div>
+                <div className="grid grid-cols-4 gap-8 mt-12 max-lg:grid-cols-2 max-md:grid-cols-1">
+                    {members.map((member) => (
+                        <div key={member.id} className="text-center">
+                            <div className="border-4 cursor-pointer border-black bg-[#ffffff] mb-4 rounded-xl ">
+                                <img src={member.src} alt="Member" />
+                            </div>
+
+                            <h1 className="font-bold leading-[2.625rem] text-[1.875rem] ">
+                                {member.Name}
+                            </h1>
+
+                            <p className="font-normal text-[1.3125rem] leading-[1.875rem] text-[rgb(12, 12, 12)] ">
+                                {member.Role}
+                            </p>
+                        </div>
+                    ))}
                 </div>
             </div>
         </>
